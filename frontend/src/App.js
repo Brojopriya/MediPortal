b0e75e9f5dca31b45ce67e588fd63bebfbb65db6
BROJOPRIYA <brojopriyanag@gmail.com>
2025-10-11 03:01:15 +0600

    updated homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,8 @@ import SignupPage from "./SignupPage";
 import LoginPage from "./LoginPage";
 import ForgotPasswordPage from "./ForgotPasswordPage";
 import { fetchDoctors, fetchServices } from "./api.js";
+import mediLogo from "./medi.png"; // path relative to App.js
+
 
 function App() {
   const [doctors, setDoctors] = useState([]);
@@ -36,6 +38,31 @@ function App() {
         <header className="App-header">
           <h1>MediPortal</h1>
         </header>
+        <header className="App-header">
+  <div className="header-container">
+    {/* Left side: Logo */}
+    <div className="logo">
+    <img src={mediLogo} alt="MediPortal Logo" className="logo-img" />
+      <span>MediPortal</span>
+    </div>
+
+    {/* Center: Navigation */}
+    <nav className="nav-links">
+      <a href="/">Home</a>
+      <a href="/doctors">Doctors</a>
+      <a href="/patients">Patients</a>
+      <a href="/appointments">Appointments</a>
+      <a href="/telemedicine">Telemedicine</a>
+    </nav>
+
+    {/* Right side: User actions */}
+    <div className="user-actions">
+      <a href="/login" className="btn-login">Login</a>
+      <a href="/signup" className="btn-signup">Sign Up</a>
+    </div>
+  </div>
+</header>
+
 
         <main>
           <Routes>
